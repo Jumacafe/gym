029571fb4b3e346665d46393db230b4980f27f6b
Jumacafe <carreajuanm@gmail.com>
2026-06-09 17:35:49 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'ironlog-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './entreno.html',
+  './ejercicios.html',
+  './progreso.html',
+  './logros.html',
+  './configuracion.html',
+  './manifest.json',
+  './css/main.css',
+  './js/data.js',
+  './js/storage.js',
+  './js/analytics.js',
+  './js/charts.js',
+  './js/icons.js',
+  './js/ui.js',
+  './js/actions.js',
+  './js/reminder.js',
+  './js/timer.js',
+  './js/init.js',
+  './assets/icon-192.png',
+  './assets/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match('./index.html')))
+  );
+});
